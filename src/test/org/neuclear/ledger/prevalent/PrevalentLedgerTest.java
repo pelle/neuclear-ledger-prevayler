42ca28f5660a95d9b86479b48a958224e57cd387
pelle <pelle>
2004-03-22 20:08:46 +0000

    Added simple ledger for unit testing and in memory use

--- src/test/org/neuclear/ledger/prevalent/PrevalentLedgerTest.java
+++ src/test/org/neuclear/ledger/prevalent/PrevalentLedgerTest.java
@@ -1,14 +1,14 @@
 package org.neuclear.ledger.prevalent;
 
-import org.neuclear.ledger.LedgerTest;
+import org.neuclear.commons.NeuClearException;
+import org.neuclear.ledger.AbstractLedgerTest;
 import org.neuclear.ledger.Ledger;
 import org.neuclear.ledger.LowlevelLedgerException;
 import org.neuclear.ledger.UnknownLedgerException;
-import org.neuclear.commons.NeuClearException;
 
 import javax.naming.NamingException;
-import java.sql.SQLException;
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,14 +17,14 @@ import java.io.IOException;
  * Time: 1:05:02 PM
  * To change this template use File | Settings | File Templates.
  */
-public class PrevalentLedgerTest extends LedgerTest {
+public class PrevalentLedgerTest extends AbstractLedgerTest {
     public PrevalentLedgerTest(String s) throws LowlevelLedgerException, UnknownLedgerException, SQLException, NamingException, IOException, NeuClearException {
         super(s);
     }
 
     public Ledger createLedger() throws LowlevelLedgerException {
         try {
-            return new PrevalentLedger("test","target/test-data/ledger/");
+            return new PrevalentLedger("test", "target/test-data/ledger/");
         } catch (IOException e) {
             throw new LowlevelLedgerException(e);
         } catch (ClassNotFoundException e) {
